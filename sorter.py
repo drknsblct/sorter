63787af57ca534988ee0a19d72dd93b9d48fa7d1
drknsblct <drknsblct@gmail.com>
2020-08-09 02:00:43 +0300

    Added deletion for duplicates

--- sorter.py
+++ sorter.py
@@ -2,19 +2,41 @@
 import os
 import shutil
 
-path = r"/Users/blackout/Downloads/"
-videos = r"/Users/blackout/Downloads/Videos/"
-photos = r"/Users/blackout/Downloads/Photos/"
-files = os.listdir(path)
+path = r"/Users/blackout/Downloads/" #program runs in this folder
+videos = r"/Users/blackout/Downloads/Media/Videos/" #path to video folder
+photos = r"/Users/blackout/Downloads/Media/Photos/" #path to image folder
 
+files = os.listdir(path) #lists content of path folder
+
+image_files = ["jpeg", "jpg", "png", "webp", "gif"]
+video_files = ["webm", "mp4"]
+
+duplicates = [x for x in range(1, 100)] #creates a list from 1 to 99
+string_duplicates = [str(x) for x in duplicates] #makes the above list numbers to strings
 
 for f in files:
     src = path + f
     try:
-        if f[-4:] == "jpeg" or f[-3:] == "jpg" or f[-3:] == "png" or f[-4:] == "webp" or f[-3:] == "gif":
-            shutil.move(src, photos)
-        elif f[-4:] == "webm" or f[-3:] == "mp4":
-            shutil.move(src, videos)
+        if (f[-7:-4:2] == "()" and f[-6] in string_duplicates) or (f[-8:-4:3] == "()" and f[-7:-5] in string_duplicates):
+              os.remove(src) #delete duplicate file extensions (jpg, png, gif, mp4)
+
+        elif (f[-8:-5:2] == "()" and f[-7] in string_duplicates) or (f[-9:-5:3] == "()" and f[-8:-6] in string_duplicates):
+            os.remove(src) #delete duplicate file extensions (jpeg, webp, webm)
+
+        elif "copy" in f:
+            os.remove(src) #delete files containing the word "copy"
+
+        elif f[-3:] in image_files or f[-4:] in image_files:
+            shutil.move(src, photos) #moves images to image folder
+
+        elif f[-3:] in video_files or f[-4:] in video_files:
+            shutil.move(src, videos) #moves videos to video folder
+
     except shutil.Error:
         pass
 print("Done!")
+
+
+
+
+
