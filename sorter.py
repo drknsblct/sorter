b2ada29d5f6ff28cc9754c4631253f987e23feca
drknsblct <drknsblct@gmail.com>
2021-03-19 01:17:27 +0200

    Reformatted whole file to use regex

--- sorter.py
+++ sorter.py
@@ -1,63 +1,51 @@
 #!/usr/bin/python
 import os
 import shutil
+import re
 
-path = r"/Users/blackout/Downloads/"  # program runs in this folder                   change this to your file path on MacOS/Linux
-videos = r"/Users/blackout/Downloads/Media/Videos/"  # path to video folder           change this to your file path on MacOS/Linux
-photos = r"/Users/blackout/Downloads/Media/Photos/"  # path to image folder           change this to your file path on MacOS/Linux
-pdf = r"/Users/blackout/Downloads/PDF" # path to pdf folder                           change this to your file path on MacOS/Linux
+# change these to your file path on MacOS/Linux
+
+path = r'/Users/blackout/Downloads/'  # program runs in this folder
+videos = r'/Users/blackout/Downloads/Media/Videos/'  # path to video folder
+photos = r'/Users/blackout/Downloads/Media/Photos/'  # path to image folder
+pdf = r'/Users/blackout/Downloads/PDF'  # path to pdf folder
 
 files = os.listdir(path)  # lists content of path folder
-
-image_files = ["jpeg", "jpg", "png", "webp", "gif"]
-video_files = ["webm", "mp4"]
-
-duplicates = [x for x in range(1, 100)]  # creates a list from 1 to 99
-string_duplicates = [str(x) for x in duplicates]  # makes the above list numbers to strings
 
 for f in files:
     src = path + f
     try:
-        if (f[-7:-4:2] == "()" and f[-6] in string_duplicates) or (
-                f[-8:-4:3] == "()" and f[-7:-5] in string_duplicates):
-            os.remove(src)  # deletes duplicate file extensions (jpg, png, gif, mp4)
+        if re.search('^\w+\(?\d+\)?\.(jpeg|jpg|png|webp|gif)', f):
+            os.remove(src)
 
-        elif (f[-12:-9:2] == "()" and f[-11] in string_duplicates) or (
-                f[-13:-9:3] == "()" and f[-12:-10] in string_duplicates):
-            os.remove(src)  # deletes jpg_orig duplicates
+        elif re.search('^\w+\(?\d+\)?\.(webm|mp4|mov)', f):
+            os.remove(src)
 
-        elif (f[-13:-10:2] == "()" and f[-12] in string_duplicates) or (
-                f[-14:-10:3] == "()" and f[-13:-11] in string_duplicates):
-            os.remove(src)  # deletes jpg_large duplicates
-
-        elif (f[-8:-5:2] == "()" and f[-7] in string_duplicates) or (
-                f[-9:-5:3] == "()" and f[-8:-6] in string_duplicates):
-            os.remove(src)  # deletes duplicate file extensions (jpeg, webp, webm)
-
-        elif "copy" in f:
+        elif 'copy' in f:
             os.remove(src)  # deletes files containing the word "copy"
 
-        elif f[-3:] in image_files or f[-4:] in image_files or ("jpg_orig" in f or "jpg_large" in f):
-            shutil.move(src, photos)  # moves images to image folder
+        elif re.search('^\w+\.(jpeg|jpg|png|webp|gif)', f):
+            shutil.move(src, photos)
 
-        elif f[-3:] in video_files or f[-4:] in video_files:
-            shutil.move(src, videos)  # moves videos to video folder
-        
+        elif re.search('^\w+\.(webm|mp4|mov)', f):
+            shutil.move(src, videos)
+
         elif 'pdf' in f:
             shutil.move(src, pdf)
-        
+
         elif "dmg" in f:
+            os.remove(src)
+
+        elif 'crdownload' in f:
             os.remove(src)
 
     except shutil.Error:
         pass
 
-# delete photo/video/pdf from Downloads if already in Photo/Video folder
+    # delete every leftover file that isn't a folder
     try:
-        if f[-3:] in image_files or f[-4:] in image_files or ("jpg_orig" in f or "jpg_large" in f) or f[-3:] in video_files or f[-4:] in video_files:
-            os.remove(src)
-        elif "pdf" in f:
+        if re.search('^\w+\.\w+\.?\w+$', f):
             os.remove(src)
     except FileNotFoundError:
         pass
-print("Done!")
+print('Done!')
