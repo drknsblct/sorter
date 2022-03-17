37a7c3ab1acf3e273634adf57c2b0ab808e1093a
drknsblct <drknsblct@gmail.com>
2022-03-17 03:41:47 +0200

    fixed bug with sorting and renamed some variables

--- sorter.py
+++ sorter.py
@@ -7,22 +7,24 @@ import time
 # change these to your file path on MacOS/Linux
 
 path = r'/Users/blackout/Downloads/'  # program runs in this folder
-videos = r'/Users/blackout/Downloads/Media/Videos/'  # path to video folder
-photos = r'/Users/blackout/Downloads/Media/Photos/'  # path to image folder
-pdf = r'/Users/blackout/Downloads/PDF'  # path to pdf folder
+videos_folder = r'/Users/blackout/Downloads/Media/Videos/'  # path to video folder
+photos_folder = r'/Users/blackout/Downloads/Media/Photos/'  # path to image folder
+pdf_folder = r'/Users/blackout/Downloads/PDF'  # path to pdf_folder folder
 
 files = os.listdir(path)  # lists content of path folder
 
 begin = time.time()
 count = 0
 deleted = 0
-photo_files = '^[\w+-]+\s?(\(\d+\))\.(jpeg|jpg|jpg_orig|jpg_large|png|webp|gif)$'
-video_files = '^[\w+-]+\s?(\(\d+\))\.(webm|mp4|mkv|mov|ts|wmv)$'
+photos = '^(.+)\.(jpeg|jpg|jpg_orig|jpg_large|png|webp|gif)$'
+videos = '^(.+)\.(webm|mp4|mkv|mov|ts|wmv)$'
+duplicate_photos = '^[\w+-]+\s?(\(\d+\))\.(jpeg|jpg|jpg_orig|jpg_large|png|webp|gif)$'
+duplicate_videos = '^[\w+-]+\s?(\(\d+\))\.(webm|mp4|mkv|mov|ts|wmv)$'
 
 # create folders and skip if they already exist
-os.makedirs(photos, exist_ok=True)
-os.makedirs(videos, exist_ok=True)
-os.makedirs(pdf, exist_ok=True)
+os.makedirs(photos_folder, exist_ok=True)
+os.makedirs(videos_folder, exist_ok=True)
+os.makedirs(pdf_folder, exist_ok=True)
 
 # count number of directories in specified path
 directories = sum(os.path.isdir(os.path.join(path, i)) for i in os.listdir(path))
@@ -34,24 +36,24 @@ for f in files:
             os.remove(src)  # deletes files containing the word "copy"
             deleted += 1
 
-        elif re.search(photo_files, f):
-            os.remove(src)  # deletes duplicate photos
+        elif re.search(duplicate_photos, f):
+            os.remove(src)  # deletes duplicate photos_folder
             deleted += 1
 
-        elif re.search(video_files, f):
-            os.remove(src)  # deletes duplicate videos
+        elif re.search(duplicate_videos, f):
+            os.remove(src)  # deletes duplicate videos_folder
             deleted += 1
 
-        elif re.search(photo_files, f):
-            shutil.move(src, photos)  # moves photos to folder
+        elif re.search(photos, f):
+            shutil.move(src, photos_folder)  # moves photos_folder to folder
             count += 1
 
-        elif re.search(video_files, f):
-            shutil.move(src, videos)  # moves videos to folder
+        elif re.search(videos, f):
+            shutil.move(src, videos_folder)  # moves videos_folder to folder
             count += 1
 
-        elif 'pdf' in f or 'epub' in f:
-            shutil.move(src, pdf)  # moves pdf to folder
+        elif 'pdf_folder' in f or 'epub' in f:
+            shutil.move(src, pdf_folder)  # moves pdf_folder to folder
             count += 1
 
     except shutil.Error:
